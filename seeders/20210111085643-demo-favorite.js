59003c7971b9b826d9945031fc48cc2ecb5a1fc6
min-gukang <route2054@gmail.com>
2021-01-11 20:56:00 +0900

    Add new column to Cocktails table(#2)

---
+++ seeders/20210111085643-demo-favorite.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Favorites', [{
+      userId: 1,
+      cocktailId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Favorites', null, {});
+  }
+};
