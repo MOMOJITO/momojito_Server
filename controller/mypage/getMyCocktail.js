da13514f25e35906e8f1b00c705b94de9a8b496a
min-gukang <route2054@gmail.com>
2021-01-12 16:51:34 +0900

    Add feature  getMyCocktail, getUserData(#5)

--- controller/mypage/getMyCocktail.js
+++ controller/mypage/getMyCocktail.js
@@ -1,3 +1,27 @@
-module.exports = (req, res) => {
-    res.send("getMyCocktail");
+const { User, Favorite } = require('../../models');
+const jwt = require('jsonwebtoken');
+const config = require('../../config/index');
+const {TOKEN_SECRET} = config;
+
+module.exports = async (req, res) => {
+  let token = req.cookies('token');
+
+  if(!token) {
+    res.state(400).json({message : 'Fail to get your cocktail(not token)'});
+  } else {
+    try {
+      let tokenData =  await jwt.verify(token, TOKEN_SECRET);
+      let userInfo = await User.findOne({
+          where : {email : tokenData.email}
+      })
+      let favorite = await Favorite.findAll({
+          where : {userId : userInfo.dataValues.id}
+      });
+      
+      favorite = favorite.map((el) => el.dataValues.cocktailId);
+      res.state(200).json({data : favorite, message : 'complete get your cocktails'});
+    } catch(err) {
+      res.state(500).json({message : 'Fail to get your cocktail'});
+    }
+  }
 };
--- controller/mypage/getUserData.js
+++ controller/mypage/getUserData.js
@@ -1,3 +1,21 @@
-module.exports = (req, res) => {
-    res.send("getUserData");
+const { User } = require('../../models');
+const jwt = require('jsonwebtoken');
+const config = require('../../config/index');
+const {TOKEN_SECRET} = config;
+
+module.exports = async (req, res) => {
+  const token = req.cookies('token');
+
+  if(!token) {
+      res.status(400).json({message: 'Fail to get User data(not token)'});
+  } else {
+      try {
+        let tokenData = jwt.verify(token, TOKEN_SECRET);
+        let userInfo = await User.findOne({
+            where : {email : tokenData.email},
+        })
+      } catch(err) {
+          res.status(500).json({message: 'Fail to get User data'});
+      }
+  }
 };
