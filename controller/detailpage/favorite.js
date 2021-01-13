070b685d3c46cd819f70aa8ac300f81ce54c4ed8
min-gukang <route2054@gmail.com>
2021-01-13 16:43:55 +0900

    Add #6-favorite(still going)

--- controller/detailpage/favorite.js
+++ controller/detailpage/favorite.js
@@ -1,3 +1,20 @@
-module.exports = (req, res) => {
-    res.send("Favorite");
+const { User, Favorite } = require('../../models');
+const jwt = require('jsonwebtoken');
+const config = require('../../config/index');
+const {TOKEN_SECRET} = config;
+
+module.exports = async (req, res) => {
+    let {cocktailId} = req.body;
+    const token = req.cookies.token;
+
+    if(!token) {
+        res.status(400).json({ message : 'Fail to add your cocktail(not token)' });
+    } else {
+        let tokenData = jwt.verify(token, TOKEN_SECRET);
+        let userInfo = await User.findOne({
+            where : { email : tokenData.email }
+        })
+
+        
+    }
 };
