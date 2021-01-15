1193a1f9110e9574a516905243e688a256f6dc7a
min-gukang <route2054@gmail.com>
2021-01-15 11:55:40 +0900

    Add deleteFavorite in mypage

--- controller/detailpage/index.js
+++ controller/detailpage/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const deleteFavorite = require('./deleteFavorite');
 const favorite = require('./favorite');
 const getBarData = require('./getBarData');
 const rating = require('./rating');
 
-router.post('/deleteFavorite', deleteFavorite);
 router.post('/favorite', favorite);
 router.get('/getBarData', getBarData);
 router.post('/rating', rating);
