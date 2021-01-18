284585558f6f675992cdd3e8a6b403109e324998
min-gukang <route2054@gmail.com>
2021-01-18 21:17:03 +0900

    Add accesstoken in auth

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const signin = require('./signin');
 const signout = require('./signout');
 const signup = require('./signup');
+const accessToken = require('./accesstoken');
 
 router.post('/signin', signin);
 router.post('/signout', signout);
 router.post('/signup', signup);
+router.get('/accesstoken', accessToken)
 
 module.exports = router;
