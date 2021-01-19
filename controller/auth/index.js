22547d0fe955001ccd9d3e15b3469bfc9683ecd5
min-gukang <route2054@gmail.com>
2021-01-19 10:37:25 +0900

    Add naveroAuth

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -5,10 +5,14 @@ const signin = require('./signin');
 const signout = require('./signout');
 const signup = require('./signup');
 const accessToken = require('./accesstoken');
+const navercallback = require('./authcallback/navercallback');
+const naverOauth = require('./naver');
 
 router.post('/signin', signin);
 router.post('/signout', signout);
 router.post('/signup', signup);
-router.get('/accesstoken', accessToken)
+router.post('/naver', naverOauth);
+router.get('/accesstoken', accessToken);
+router.get('/navercallback', navercallback);
 
 module.exports = router;
