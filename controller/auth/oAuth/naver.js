b03da3d5eab18e20cc2fed9970e6dd96141be53b
min-gukang <route2054@gmail.com>
2021-01-20 15:54:48 +0900

    Fix oAuth folder constructor

--- controller/auth/oAuth/naver.js
+++ controller/auth/oAuth/naver.js
@@ -1,26 +1,24 @@
-const { User, Favorite } = require('../../models');
+const { User, Favorite } = require('../../../models');
 const fetch = require('node-fetch');
 const jwt = require('jsonwebtoken');
-const config = require('../../config/index');
+const config = require('../../../config/index');
 const { TOKEN_SECRET } = config;
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { authorizationCode } = req.body;
-  console.log(authorizationCode);
 
-  let url = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${config.KAKAO_API_KEY}&redirect_uri=http://localhost:5000/auth/kakaocallback&code=${authorizationCode}`;
+  let url = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${config.NAVER_API_KEY}&client_secret=${config.NAVER_SECRET}&redirect_uri=http://localhost:5000/auth/navercallback&code=${authorizationCode}&state=rara`;
 
   fetch(url, {
-    method: 'POST',
+    method: 'GET',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      'X-Naver-Client-Id': config.NAVER_API_KEY,
+      'X-Naver-Client-Secret': config.NAVER_SECRET,
     },
-    // body : JSON.stringify(data)
   })
     .then((res) => res.json())
     .then((data) => {
-      let uri = 'https://kapi.kakao.com/v2/user/me?]';
-      fetch(uri, {
+      fetch('https://openapi.naver.com/v1/nid/me', {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${data.access_token}`,
@@ -28,29 +26,26 @@ module.exports = (req, res) => {
       })
         .then((res) => res.json())
         .then(async (json) => {
-          //email : json.kakao_account.email
           console.log(json);
+
           let checkEmail = await User.findOne({
-            where: { email: json.kakao_account.email },
+            where: { email: json.response.email },
           });
 
           if (!checkEmail) {
             await User.create({
-              email: json.kakao_account.email,
-              nickname: json.properties.nickname,
-              profile:
-                'https://avatars1.githubusercontent.com/u/47313528?s=88&v=4',
+              email: json.response.email,
+              nickname: json.response.nickname,
+              profile: json.response.profile_image,
             });
-            let token = jwt.sign(
-              { email: json.kakao_account.email },
-              TOKEN_SECRET,
-            );
+            //여기서 db에 이메일 저장하고 jwt토큰발급.. 그리고 쿠키로 전송
+            let token = jwt.sign({ email: json.response.email }, TOKEN_SECRET);
 
             let userInfo = await User.findOne({
               attributes: {
                 exclude: ['createdAt', 'updatedAt', 'password'],
               },
-              where: { email: json.kakao_account.email },
+              where: { email: json.response.email },
             });
 
             res
@@ -60,21 +55,18 @@ module.exports = (req, res) => {
                 data: {
                   accessToken: token,
                   userInfo: userInfo.dataValues,
-                  cocktailList,
+                  cocktailList: [],
                 },
                 message: 'Sign In completed',
               });
           } else {
-            let token = jwt.sign(
-              { email: json.kakao_account.email },
-              TOKEN_SECRET,
-            );
+            let token = jwt.sign({ email: json.response.email }, TOKEN_SECRET);
 
             let userInfo = await User.findOne({
               attributes: {
                 exclude: ['createdAt', 'updatedAt', 'password'],
               },
-              where: { email: json.kakao_account.email },
+              where: { email: json.response.email },
             });
 
             let cocktailList = await Favorite.findAll({
