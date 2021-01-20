7be4abf5e915d58016e324a826dbe00bc28deb63
min-gukang <route2054@gmail.com>
2021-01-20 11:36:52 +0900

    Add kakaoOauth

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -6,13 +6,17 @@ const signout = require('./signout');
 const signup = require('./signup');
 const accessToken = require('./accesstoken');
 const navercallback = require('./authcallback/navercallback');
+const kakaocallback = require('./authcallback/kakaocallback');
 const naverOauth = require('./naver');
+const kakaoOauth = require('./kakao');
 
 router.post('/signin', signin);
 router.post('/signout', signout);
 router.post('/signup', signup);
 router.post('/naver', naverOauth);
+router.post('/kakao', kakaoOauth)
 router.get('/accesstoken', accessToken);
 router.get('/navercallback', navercallback);
+router.get('/kakaocallback', kakaocallback);
 
 module.exports = router;
